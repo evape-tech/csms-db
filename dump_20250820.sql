1d27ffd6afce7b76019a7484786cb49a763b9b84
evape-tech <evapetechofficial@gmail.com>
2025-11-03 11:21:33 +0800

    [add] 新增現金支付選項

--- dump_20250820.sql
+++ dump_20250820.sql
@@ -572,10 +572,11 @@ CREATE TABLE `billing_channels` (
 
 INSERT INTO `billing_channels` (`name`, `code`, `status`, `config`)
 VALUES
+('現金支付', 'cash', 1, NULL),
 ('信用卡', 'credit_card', 1, NULL),
 ('Line Pay', 'line_pay', 1, NULL),
 ('悠遊付', 'easy_wallet', 1, NULL),
-('全盈 pay', 'plus_pay', 1, NULL)
+('全盈 pay', 'plus_pay', 1, NULL),
 
 ON DUPLICATE KEY UPDATE
   `name` = VALUES(`name`),
